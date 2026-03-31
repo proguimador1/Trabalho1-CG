9163b750c6a75a91bff27d09e2d566c8926a9f4f
João Victor dos Santos Sales <jvsantossales@hotmail.com>
2026-03-30 21:48:34 -0300

    Coloração do mapa - Desenho Incompleto

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ while True:
                 else:
                     screen = pygame.display.set_mode((width,height))
 
-    #screen.fill((0,0,0))
+    screen.fill((178, 116, 0))
 
     mp.desenhar_mapa(screen)
 
--- mapa.py
+++ mapa.py
@@ -14,45 +14,74 @@ def desenhar_mapa(screen):
     geladeira = [(800,345),(950,345),(950,530),(800,530)]
     macaneta1 = [(810,415),(830,415),(830,423),(810,423)]
     macaneta2 = [(810,464),(830,464),(830,472),(810,472)]
-    patreleira = [(550,490),(720,490),(720,505),(550,505)]
+    pateleira = [(550,490),(720,490),(720,505),(550,505)]
     suporte1_prateleira =[(580,505),(590,505),(590,515), (580,515)]
     suporte2_prateleira = [(680,505), (690, 505), (690, 515), (680, 515)]
     microondas = [(565,445), (665,445), (665,490), (565,490)]
     vidro_microondas = [(575,455),(630,455),(630,480),(575,480)]
+    teto = [(0,0),(1000,0),(1000,125),(0,125)]
 
     #APLICAÇÃO DOS POLÍGONOS
-    pr.polygon(screen, balcao1, (255,255,255))
-    pr.polygon(screen, balcao2, (255,255,255))
+    pr.polygon(screen, balcao1, (0,0,0))
+    pr.polygon(screen, balcao2, (0,0,0))
     pr.polygon(screen, janela, (255,255,255))
     pr.polygon(screen, porta_maior, (255,255,255))
-    pr.polygon(screen, vitrine, (255,255,255))
-    pr.polygon(screen, caixa, (255,255,255))
-    pr.polygon(screen, geladeira, (255,255,255))
-    pr.polygon(screen, macaneta1, (255,255,255))
-    pr.polygon(screen, macaneta2, (255,255,255))
-    pr.polygon(screen, patreleira, (255,255,255))
-    pr.polygon(screen, suporte1_prateleira, (255,255,255))
-    pr.polygon(screen, suporte2_prateleira, (255,255,255))
-    pr.polygon(screen, microondas, (255,255,255))
+    pr.polygon(screen, vitrine, (0,0,0))
+    pr.polygon(screen, caixa, (0,0,0))
+    pr.polygon(screen, geladeira, (0,0,0))
+    pr.polygon(screen, macaneta1, (0,0,0))
+    pr.polygon(screen, macaneta2, (0,0,0))
+    pr.polygon(screen, pateleira, (0,0,0))
+    pr.polygon(screen, suporte1_prateleira, (0,0,0))
+    pr.polygon(screen, suporte2_prateleira, (0,0,0))
+    pr.polygon(screen, microondas, (0,0,0))
     pr,polygon(screen, vidro_microondas, (255,255,255))
+    pr.polygon(screen, teto, (0,0,0))
 
     #LINHA 1 E 2 DA GELADEIRA
-    pr.line(screen, (800,435), (950,435), (255,255,255))
-    pr.line(screen, (800,440), (950,440), (255,255,255))
+    pr.line(screen, (800,435), (950,435), (0,0,0))
+    pr.line(screen, (800,440), (950,440), (0,0,0))
     
     #LINHAS DA VITRINE
-    pr.line(screen, (0,455), (45,455), (255,255,255))
-    pr.line(screen, (0, 480),(45,480), (255,255,255))
-    pr.line(screen, (0,505), (45,505), (255,255,255))
-
-    #TETO
-    pr.line(screen, (0,125), (1000,125), (255,255,255))
-
+    pr.line(screen, (0,455), (45,455), (0,0,0))
+    pr.line(screen, (0, 480),(45,480), (0,0,0))
+    pr.line(screen, (0,505), (45,505), (0,0,0))
+    
     #Boca do caixa
-    pr.circle(screen, 17, (102,510), (255,255,255))
+    pr.circle(screen, 17, (102,510), (0, 0, 0))
+    pr.scan_line_ellipsis(screen, 17,17, (102,510), (178, 116, 0))
     # Relógio de parede
-    pr.circle(screen, 30, (240,360), (255,255,255))
-    #pr.scan_line_polygon(screen, balcao1, (255,255,255))
+    pr.circle(screen, 30, (240,360), (0,0,0))
+    pr.circle(screen, 25, (240,360), (255,255,255))
+    pr.scan_line_ellipsis(screen, 30,30, (240,360), (0,0,0))
+    pr.scan_line_ellipsis(screen, 25,25, (240,360), (255,255,255))
+
+
+    #COLORAÇÃO
+    """ TABELA DE CORES:
+        Prata (silver): RGB(192, 192, 192)
+        Marrom claro: RGB(181, 101, 29)
+        Branco creme: RGB(245, 245, 220)
+        Vidro (translúcido claro): RGB(200, 220, 230)
+        Vermelho claro: RGB(255, 102, 102)
+        Laranja: RGB(255, 165, 0)     
+        Cinza (gray padrão): RGB(128, 128, 128)"""
+    pr.scan_line_polygon(screen, balcao1, (90, 50, 15))
+    pr.scan_line_polygon(screen, balcao2, (192, 192, 192))
+    pr.scan_line_polygon(screen, janela, (0,0,0))
+    pr.scan_line_polygon(screen, porta_maior, (0,0,0))
+    pr.scan_line_polygon(screen, caixa, (200, 220, 230))
+    pr.scan_line_polygon(screen, geladeira, (240, 80, 80))
+    pr.scan_line_polygon(screen, macaneta1, (128, 128, 128))
+    pr.scan_line_polygon(screen, macaneta2, (128, 128, 128))
+    pr.scan_line_polygon(screen, suporte1_prateleira, (181, 101, 29))
+    pr.scan_line_polygon(screen, suporte2_prateleira, (181, 101, 29))
+    pr.scan_line_polygon(screen, pateleira, (181, 101, 29))
+    pr.scan_line_polygon(screen, microondas, (245, 245, 220))
+    pr.scan_line_polygon(screen, vitrine, (245, 245, 220))
+    pr.scan_line_polygon(screen, vidro_microondas, (200, 220, 230))
+    pr.scan_line_polygon(screen, teto, (120, 70, 20))
+
 
     #LINHAS DO BALCÃO 2 (BALCÃO DE SALGADOS)
     #Primeira sequência
