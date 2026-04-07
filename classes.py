95745b84353957bc33c13323321c325942683092
Guilherme Andrade <guilherme.programmer1@gmail.com>
2026-04-07 20:27:21 -0300

    Primeira tentativa de implementação de um relógio

--- classes.py
+++ classes.py
@@ -3,4 +3,25 @@ from transforms import create_transform
 from pygame import Surface
 
 class Clock:
-    ...
+    def __init__(self, screen:Surface, color, fix_point, other_point, radius):
+        self.screen = screen
+        self.fix_point = fix_point
+        self.other_point = other_point
+        self.radius = radius
+        self.color = color
+
+    def draw_clock(self):
+        pr.circle(self.screen, self.radius, self.fix_point, self.color)
+        pr.line(self.screen, self.fix_point, self.other_point, self.color)
+
+    def run_clock(self):
+        p1 = self.fix_point
+
+        new_p1, self.other_point = create_transform((p1,self.other_point), theta=10)
+
+        deltax = p1[0]-new_p1[0]
+        deltay = p1[1]-new_p1[1]
+
+        _, self.other_point = create_transform((new_p1, self.other_point), delta=(deltax, deltay))
+
+        self.draw_clock()
--- test_canvas.py
+++ test_canvas.py
@@ -11,13 +11,15 @@ heigth = 800
 pygame.init()
 
 screen = pygame.display.set_mode((width, heigth))
-circulo = {'center': (400, 400), 'radius': 300}
-linha = [(400, 400), (500, 600)]
 
-relogio = Clock(circulo, linha)
+fix_point = (400,400)
+other_point = (500, 600)
+radius = 250
+relogio = Clock(screen, (250,250,250), fix_point, other_point, radius)
+
+relogio.draw_clock()
 
 while True:
-    screen.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -26,9 +28,6 @@ while True:
         # Detecta o pressionar da tecla Espaço
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                #relogio.run_clock(screen, (250,250,250))
-                ...
-
-        #relogio.draw_clock(screen, (250,250,250))
+                relogio.run_clock()
     
     pygame.display.update()
