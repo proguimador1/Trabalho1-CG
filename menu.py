fd59306ec969d28706feea4af10ff4ae751e02c7
Rafael <monteiro.cavalcante@aluno.uece.br>
2026-04-17 13:46:18 -0300

    Atualizando menu.py

--- menu.py
+++ menu.py
@@ -6,46 +6,7 @@ import sys
 pygame.init()
 largura, altura = 800, 600
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("Set Pixel")
-
-
-def setPixel(superficie, x, y, cor):
-    superficie.set_at((x, y), cor)
-
-
-def lineBresenham(x0, y0, x1, y1, color):
-    dx = abs(x1 - x0)
-    dy = abs(y1 - y0)
-    sx = 1
-    if x0 < x1:
-        sx = 1
-    else:
-        sx = -1
-    sy = 1
-    if y0 < y1:
-        sy = 1
-    else:
-        sy = -1
-    err = dx - dy
-    while True:
-        setPixel(tela, x0, y0, color)
-        if x0 == x1 and y0 == y1:
-            break
-        e2 = 2 * err
-        if e2 > -dy:
-            err -= dy
-            x0 += sx
-        if e2 < dx:
-            err += dx
-            y0 += sy
-
-
-def drawPolygon(vertices, color):
-    n = len(vertices)
-    for i in range(n):
-        x0, y0 = vertices[i]
-        x1, y1 = vertices[(i + 1) % n]
-        lineBresenham(x0, y0, x1, y1, color)
+pygame.display.set_caption("menu")
 
 
 def centro_botao(vertices):
@@ -70,7 +31,7 @@ while rodando:
     tela.fill((255, 165, 0))
 
     vertices = [(250, 300), (550, 300), (550, 350), (250, 350)]
-    drawPolygon(vertices, (0, 0, 0))
+    polygon(tela, vertices, (0, 0, 0))
     t1 = font.render("Opção 1", True, (0, 0, 0))
     cx, cy = centro_botao(vertices)
     largura = t1.get_width()
@@ -78,7 +39,7 @@ while rodando:
     tela.blit(t1, (cx - largura // 2, cy - altura // 2))
 
     vertices = [(270, 430), (530, 430), (530, 470), (270, 470)]
-    drawPolygon(vertices, (0, 0, 0))
+    polygon(tela, vertices, (0, 0, 0))
     t2 = font.render("Sair", True, (0, 0, 0))
     cx, cy = centro_botao(vertices)
     largura = t2.get_width()
