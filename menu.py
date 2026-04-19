8aa3a0d268564172d1a82c6eafe16eaecaa2f805
Rafael <monteiro.cavalcante@aluno.uece.br>
2026-04-19 17:45:49 -0300

    Atualizando menu.py (x)

--- menu.py
+++ menu.py
@@ -24,14 +24,16 @@ while rodando:
             rodando = False
         if evento.type == pygame.MOUSEBUTTONUP:
             x, y = evento.pos
-            if 250 <= x <= 550 and 300 <= y <= 350:
+            if 250 <= x <= 550 and 250 <= y <= 300:
                 print("Opção 1 selecionada")
             elif 270 <= x <= 530 and 430 <= y <= 470:
                 rodando = False
     tela.fill((255, 165, 0))
 
-    vertices = [(250, 300), (550, 300), (550, 350), (250, 350)]
+    vertices = [(250, 250), (550, 250), (550, 300), (250, 300)]
     polygon(tela, vertices, (0, 0, 0))
+    flood_fill(tela, (255, 255), (128, 128, 128))
+
     t1 = font.render("Opção 1", True, (0, 0, 0))
     cx, cy = centro_botao(vertices)
     largura = t1.get_width()
@@ -40,6 +42,8 @@ while rodando:
 
     vertices = [(270, 430), (530, 430), (530, 470), (270, 470)]
     polygon(tela, vertices, (0, 0, 0))
+    flood_fill(tela, (275, 435), (128, 128, 128))
+
     t2 = font.render("Sair", True, (0, 0, 0))
     cx, cy = centro_botao(vertices)
     largura = t2.get_width()
@@ -50,6 +54,24 @@ while rodando:
     largura = text_surface.get_width()
     altura = text_surface.get_height()
     tela.blit(text_surface, (400 - largura / 2, 100))
+
+    vertices = [
+        (330, 325),
+        (370, 325),
+        (400, 355),
+        (430, 325),
+        (470, 325),
+        (410, 365),
+        (470, 405),
+        (430, 405),
+        (400, 375),
+        (370, 405),
+        (330, 405),
+        (390, 365),
+    ]
+    polygon(tela, vertices, (255, 0, 0))
+    flood_fill(tela, (340, 327), (0, 0, 0))
+
     pygame.display.flip()
 
 pygame.quit()
